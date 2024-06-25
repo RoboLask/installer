8a3d7d891f55f766a53ce7cf37f2f410ffbb8c25
RoboLask <129116499+RoboLask@users.noreply.github.com>
2024-06-25 16:52:44 +0200

    Update kde.sh

--- kde.sh
+++ kde.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-echo "youe choice"
+printf "Select your preferred desktop environment:\n1.Plasma\n2.GNOME\n3.Hyprland(Wayland compositor)\nenter your choice(in number):"
 read user_input
 
-if [ "$user_input" -eq 1 ]; then
-    sudo pacman -S plasma konsole sddm dolphin
-    systemctl enable sddm.service
+if [ "$user_input" -eq "1" ]; then
+    sudo pacman -Rns --noconfirm gnome
+    sudo pacman -S --noconfirm kde plasma sddm
+    systemctl enable sddm
+elif [ "$user_input" -eq "2"]; then
+    systemctl enable gdm
+elif [ "$user_input" -eq "3" ]; then
+    sudo pacman -Rns --noconfirm gnome
+    sudo pacman -S --noconfirm hyprland sddm
+    systemctl enable sddm
 else
-    echo "sd"
+    echo "Wrong number typed, Aboring..."
 fi
